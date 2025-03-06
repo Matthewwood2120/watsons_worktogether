2a5c45dd1983460a1666b7e1b7f94f4317fdae28
Matthewwood2120 <mw70079p@pace.edu>
2025-03-06 00:16:02 -0500

    Update length_last_word.py
    this is my change to length last word - Matthew wood

--- length-lastword/length_last_word.py
+++ length-lastword/length_last_word.py
@@ -1,3 +1,11 @@
+#Matthew Wood
+
 def length_of_last_word(word):
-  ''' Returns the length of the last word in a string '''
-  return True
+    # ''' Returns the length of the last word in a string '''
+    words=word.split()
+    if words:
+        return len(words[-1])
+    return 0
+
+user_input = input("Enter a string: ")
+print(length_of_last_word(user_input))
